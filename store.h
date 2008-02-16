15a50f8d4246b6e160932d0d11f11bfb7db8c678
jonb <jonb@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2008-02-16 20:30:49 +0000

    mod_tile: Update to a new meta tile scheme which stores all sub-tiles in a single .meta file. The PNG files are extracted from this on the fly by mod_tile. This is more efficient in disk space and inode usage
    git-svn-id: http://svn.openstreetmap.org/applications/utils/mod_tile@6894 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ store.h
@@ -0,0 +1,42 @@
+#ifndef STORE_H
+#define STORE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdlib.h>
+
+int tile_read(int x, int y, int z, char *buf, int sz);
+
+#define META_MAGIC "META"
+//static const char meta_magic[4] = { 'M', 'E', 'T', 'A' };
+
+struct entry {
+    int offset;
+    int size;
+};
+
+struct meta_layout {
+    char magic[4];
+    int count; // METATILE ^ 2
+    int x, y, z; // lowest x,y of this metatile, plus z
+    struct entry index[]; // count entries
+    // Followed by the tile data
+    // The index offsets are measured from the start of the file
+};
+
+
+int read_from_file(int x, int y, int z, char *buf, size_t sz);
+int read_from_meta(int x, int y, int z, char *buf, size_t sz);
+
+void process_meta(int x, int y, int z);
+void process_pack(const char *name);
+void process_unpack(const char *name);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
