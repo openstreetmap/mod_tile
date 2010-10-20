openstreetmap/mod_tile

commit 1f3d2ddab6bb3ef5b492bf73089c61e718a22ee5

Do not spawn render threads when we're not actually going to rerender anything.
git-svn-id: http://svn.openstreetmap.org/applications/utils/mod_tile@21645 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

render_expired.c
@@ -333,6 +333,7 @@ int main(int argc, char **argv)
     int numThreads = 1;
     int deleteFrom = -1;
     int touchFrom = -1;
+    int doRender = 0;
     int i;
 
     // Initialize touchFrom timestamp
@@ -476,7 +477,11 @@ int main(int argc, char **argv)
 
     gettimeofday(&start, NULL);
 
-    spawn_workers(numThreads, spath);
+    if (minZoom < touchFrom || minZoom < deleteFrom) {
+        // No need to spawn render threads, when we're not actually going to rerender tiles
+        spawn_workers(numThreads, spath);
+	doRender = 1;
+    }
 
     while(!feof(stdin)) 
     {
@@ -546,7 +551,7 @@ int main(int argc, char **argv)
                     utime(name, &touchTime);
                     num_touch++;
                 }
-                else
+                else if (doRender)
                 {
                     printf("render: %s\n", name);
                     enqueue(name);
@@ -575,7 +580,9 @@ int main(int argc, char **argv)
         }
     }
 
-    finish_workers(numThreads);
+    if (doRender) {
+        finish_workers(numThreads);
+    }
 
     gettimeofday(&end, NULL);
     printf("\nTotal for all tiles rendered\n");

commit 1387730e4a2b85e16990d2e0aeda2fb9997f3e8b

Fix bug introduced in r21645, which caused render_expire to never actually rerender anything. Noted on the dev ML.
git-svn-id: http://svn.openstreetmap.org/applications/utils/mod_tile@23729 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

render_expired.c
@@ -477,7 +477,9 @@ int main(int argc, char **argv)
 
     gettimeofday(&start, NULL);
 
-    if (minZoom < touchFrom || minZoom < deleteFrom) {
+    if (   ( touchFrom != -1 && minZoom < touchFrom )
+        || (deleteFrom != -1 && minZoom < deleteFrom)
+        || ( touchFrom == -1 && deleteFrom == -1) ) {
         // No need to spawn render threads, when we're not actually going to rerender tiles
         spawn_workers(numThreads, spath);
 	doRender = 1;