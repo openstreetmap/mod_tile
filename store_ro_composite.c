3b639e19aefc688125f834bb28860bf072516d35
Jeffrey C. Ollie <jeff@ocjtech.us>
2013-04-05 00:00:40 -0500

    Add copyright and permission info.

--- store_ro_composite.c
+++ store_ro_composite.c
@@ -1,3 +1,22 @@
+/*
+Copyright © 2013 mod_tile contributors
+
+This file is part of mod_tile.
+
+mod_tile is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 2 of the License, or (at your
+option) any later version.
+
+mod_tile is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with mod_tile.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "config.h"
 #include <stdio.h>
 #include <stdlib.h>
