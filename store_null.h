c07adeebf80bbe756ee8da2ae3425f917d1d2739
Matt Amos <zerebubuth@gmail.com>
2013-05-13 19:35:01 +0100

    Added simple 'null' storage, which might be useful for benchmarking or testing.

---
+++ store_null.h
@@ -0,0 +1,16 @@
+#ifndef STORE_NULL_H
+#define STORE_NULL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "store.h"
+
+   struct storage_backend * init_storage_null();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* STORE_NULL_H */
