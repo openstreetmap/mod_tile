5dffe058e6c9e5781bc8f9dd2f2c7aca010797fe
Dirk Stoecker <github@dstoecker.de>
2013-12-05 16:35:49 +0100

    fix gcc errors

--- includes/store_file.h
+++ includes/store_file.h
@@ -8,7 +8,8 @@ extern "C" {
 #include "store.h"
     
     struct storage_backend * init_storage_file(const char * tile_dir);
-        
+    int xyzo_to_meta(char *path, size_t len, const char *tile_dir, const char *xmlconfig, const char *options, int x, int y, int z);
+
 #ifdef __cplusplus
 }
 #endif
