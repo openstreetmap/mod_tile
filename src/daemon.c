2a9f4db1bf19332f5db9d1c30fc45f5578e90aa7
Michael Schmiedgen <root@maps.takwa.de>
2021-12-30 12:59:18 +0100

    FreeBSD uses diffenent include path for iniparser.h Some Linux distros may also and this can be easily adapted, see https://bugzilla.redhat.com/show_bug.cgi?id=1635706

--- src/daemon.c
+++ src/daemon.c
@@ -43,7 +43,11 @@
 #include "g_logger.h"
 
 // extern "C" {
+#ifdef __FreeBSD__
+#include <iniparser.h>
+#else
 #include <iniparser/iniparser.h>
+#endif
 // }
 
 #define PIDFILE "/run/renderd/renderd.pid"
