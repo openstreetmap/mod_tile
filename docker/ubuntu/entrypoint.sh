a94fbcb480c6d6be821731d16123375e87ae6112
David Hummel <6109326+hummeltech@users.noreply.github.com>
2024-03-05 11:57:30 -0700

    Fix full Ubuntu map service

--- docker/ubuntu/entrypoint.sh
+++ docker/ubuntu/entrypoint.sh
@@ -20,11 +20,9 @@ then
 
     cd /data/style
 
-    python3 ./scripts/get-external-data.py -c /data/style/external-data.yml -D /data/style/data
+    python3 scripts/get-external-data.py -c /data/style/external-data.yml -D /data/style/data
 
-    ./scripts/get-fonts.sh
-
-    mv fonts/* /usr/share/fonts/
+    scripts/get-fonts.sh
 
     psql --host "${PGHOST}" --user "${PGUSER}" --dbname "${PGDATABASE}" --command "CREATE EXTENSION postgis;"
     psql --host "${PGHOST}" --user "${PGUSER}" --dbname "${PGDATABASE}" --command "CREATE EXTENSION hstore;"
@@ -33,19 +31,31 @@ then
 
     curl --location "${DOWNLOAD_PBF:-http://download.geofabrik.de/asia/vietnam-latest.osm.pbf}" --output /data/region.osm.pbf
 
-    osm2pgsql --host "${PGHOST}" --username "${PGUSER}" --database "${PGDATABASE}" --create --slim -G --hstore \
-        --tag-transform-script /data/style/openstreetmap-carto.lua \
+    osm2pgsql \
+        --create \
+        --database "${PGDATABASE}" \
+        --host "${PGHOST}" \
+        --hstore \
         --number-processes "$(nproc)" \
+        --slim \
+        --tag-transform-script /data/style/openstreetmap-carto.lua \
+        --user "${PGUSER}" \
+        -G \
         -S /data/style/openstreetmap-carto.style \
         /data/region.osm.pbf
 
     psql --host "${PGHOST}" --user "${PGUSER}" --dbname "${PGDATABASE}" --file /data/style/indexes.sql
 
     carto /data/style/project.mml > /data/style/mapnik.xml
-    sed -i 's#/usr/share/renderd/example-map/mapnik.xml#/data/style/mapnik.xml#g' /etc/renderd.conf
-    sed -i 's/URI=/MAXZOOM=20\nMINZOOM=0\URI=/g' /etc/renderd.conf
-    sed -i 's/maxZoom: 12/maxZoom: 20/g' /usr/share/renderd/example-map/index.html
+    sed \
+        -e 's#/usr/share/renderd/example-map/mapnik.xml#/data/style/mapnik.xml#g' \
+        -e 's/URI=/MAXZOOM=20\nMINZOOM=0\nURI=/g' \
+        -e 's#font_dir=/usr/share/fonts#font_dir=/data/style/fonts#g' \
+        /etc/renderd.conf > /data/renderd.conf
 fi
 
+sed -i 's#/etc/renderd.conf#/data/renderd.conf#g' /etc/apache2/sites-enabled/renderd-example-map.conf
+sed -i 's/maxZoom: 12/maxZoom: 20/g' /usr/share/renderd/example-map/index.html
+
 apachectl -e debug -k start
-renderd --foreground
+renderd --config /data/renderd.conf --foreground
